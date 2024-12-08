a47f929bdd3b66b386505a6373d91aa2a6a53043
Marco <mbarozzi@gmail.com>
2024-12-08 11:32:44 -0300

    componente typography

---
+++ src/app/components/Typography/Typography.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export type TypographyProps = {
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3';
+  children: React.ReactNode;
+  element?: keyof JSX.IntrinsicElements;
+} & JSX.IntrinsicElements["p"] 
+
+const variantClassMap = {
+  variant: {
+    primary: 'text-gray-primary',
+    secondary: 'text-gray-secondary',
+    tertiary: 'text-gray-tertiary',
+  },
+  size: {
+    xs: 'text-xs',
+    sm: 'text-sm',
+    md: 'text-md',
+    xl: 'text-xl',
+    title1: 'text-txl',
+    title2: 'text-tlg',
+    title3: 'text-tmd',
+  },
+};
+ 
+const Typography = ({
+  variant = "primary", 
+  size = "md" ,
+  children,
+  element = "p",
+  ...rest
+}: TypographyProps) => {
+  const Element = element as React.ElementType;
+
+  return (
+    <Element {...rest}
+    className={`
+      ${variantClassMap.variant[variant]}
+      ${variantClassMap.size[size]}
+    `}
+    >
+      {children}
+    </Element>
+  );
+};
+
+export default Typography;
