93de4471aa1fb5bf5b608421207b648e33af1bdb
Marco <mbarozzi@gmail.com>
2024-12-10 12:39:49 -0300

    componente composto Notice

--- src/app/test-components/page.tsx
+++ src/app/test-components/page.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import Divider from '../components/Divider/Divider'
 import Box from '../components/Box/Box'
-
+import Notice from '../components/Notice/Notice'
 const Home = () => {
   return (
     <div>
       <h1>Testing Components</h1>
       <Divider>Hello Divider</Divider>
       <Box
-          filledBackground= {true}  
-          rounded= {true} 
-          type= "alert"
-          className='w-1/2' 
-      
+        filledBackground={true}
+        rounded={true}
+        type='alert'
+        className='w-1/2'
       >
         Conteúdo Box com Alert
       </Box>
+      <Notice message='alo' type='success'>
+        <div className={'mx-auto'}>
+          <p>Este é o conteúdo passado como children para o Notice.</p>
+          <button className='bg-blue-500 text-white rounded p-2'>Ação</button>
+        </div>
+      </Notice>
     </div>
   )
 }
