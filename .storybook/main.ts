314e4600ff5fc713624f90efd93258ba0bcd77b6
Marco <mbarozzi@gmail.com>
2024-12-12 05:29:54 -0300

    conclusão do curso

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,21 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
-    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+  staticDirs: ["../public"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
